fea1f9edf0ea7704216207f65402ec84bdb05cca
Ryan Vlaming <ryanvlaming@Ryans-MacBook-Pro.local>
2018-03-12 22:47:50 +0100

    add some unsafe untested code

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # njodmoler
 node mailer wrapper for use with Kit and Kube
+
+## installing
+
+for npm
+
+```bash
+$ npm install nodemailer --save
+$ cd app/app_addons
+$ git clone git@github.com:Kube-f/njodmoler.git
+```
+
+for yarn
+
+```bash
+$ yarn add nodemailer
+$ cd app/app_addons
+$ git clone git@github.com:Kube-f/njodmoler.git
+```
+
+Finally add the following variables to your `.env` file and fill them out accordingly
+
+```
+[nodemailer]
+EMAIL_FROM=
+EMAIL_HOST=
+EMAIL_PORT=
+EMAIL_SECURE=
+```
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-"use strict"
+import nodemailerModule from 'nodemailer';
+import Promise from 'bluebird';
+
+export default function nodemailer(kube) {
+    const nodemailer = kube.namespace('nodemailer');
+
+    Promise.promisifyAll(nodemailerModule, { suffix: 'Promise'});
+
+    nodemailer.def(function setupAccount() {
+        const defaultHost = 'smtp.ethereal.email';
+        const host = process.env.EMAIL_HOST || defaultHost;
+
+        nodemailerModule.createTestAccount()
+            .then(function handleTestAccount(account) {
+                kube.logger.trace({ account }, 'created ethereal email acocunt');
+            })
+            .catch(function handleError(error) {
+                kube.logger.error({ error }, 'Error creating ethereal account');
+            });
+    });
+}
